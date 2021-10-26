d74d3b124058c91194ef60402d3c93c3acbb0271
Nguyễn Thành <thanhh16012k@gmail.com>
2021-10-26 08:29:02 +0700

    Add project files.

---
+++ BTL_QLBH/Models/QLBHDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+
+namespace BTL_QLBH.Models
+{
+    public partial class QLBHDbContext : DbContext
+
+    {
+        public QLBHDbContext() : base("name=QLBHDbContext")
+        {
+        }
+   
+        public virtual DbSet<Person> Persons { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>()
+            .Property(e => e.PersonID)
+            .IsUnicode(false);
+        }
+    }
+}
