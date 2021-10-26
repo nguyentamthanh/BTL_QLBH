87138ff3365c83577f77ea3d9c8216c7b3388edf
Nguyễn Thành <thanhh16012k@gmail.com>
2021-10-26 09:40:54 +0700

    26_10_1

---
+++ BTL_QLBH/Controllers/PersonController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BTL_QLBH.Models;
+using BTL_QLBH.Models.Process;
+
+
+namespace BTL_QLBH.Controllers
+{
+    public class PersonController : Controller
+    {
+        private QLBHDbContext db = new QLBHDbContext();
+        private StringProcess strPro = new StringProcess();
+
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View(db.Persons.ToList());
+        }
+
+        // GET: Person/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.Persons.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            var model = db.Persons.ToList();
+            if (model.Count == 0)
+            {
+                ViewBag.CustomerID = "PER001";
+            }
+            else
+            {
+                //lấy ra mã KH mới nhất 
+                var idPerson = model.OrderByDescending(m => m.PersonID).FirstOrDefault().PersonID;
+                var newKey = strPro.AutoGenerateKey(idPerson);
+                ViewBag.PersonID = newKey;
+
+            }
+            return View();
+
+        }
+
+        // POST: Person/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PersonID,PersonName")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Persons.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+        // GET: Person/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.Persons.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // POST: Person/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PersonID,PersonName")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+
+        // GET: Person/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.Persons.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // POST: Person/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Person person = db.Persons.Find(id);
+            db.Persons.Remove(person);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
