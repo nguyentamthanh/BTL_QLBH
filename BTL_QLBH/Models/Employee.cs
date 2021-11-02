234aefee4844e22c862854cae9437d2419a445f1
Nguyễn Thành <thanhh16012k@gmail.com>
2021-11-02 07:13:03 +0700

    2/11

---
+++ BTL_QLBH/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_QLBH.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        public string EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+    }
+}
